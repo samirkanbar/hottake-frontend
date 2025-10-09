70d39ae76ad8d4ce5238c39698328bb74eb87686
Samir Kanbar <sam.kbr06@gmail.com>
2025-10-09 19:12:08 -0400

    working template

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import {SafeAreaView} from 'react-native-safe-area-context';
+import React from 'react'
+
+const index = () => {
+
+    const numActivities = 0;
+
+    return (
+        <SafeAreaView>
+            <Text>hello world</Text>
+        </SafeAreaView>
+    )
+}
+
+export default index
