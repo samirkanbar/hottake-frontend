602d8e72bb9dfcf2155600ddf1621fe9da610be6
Samir Kanbar <sam.kbr06@gmail.com>
2025-10-13 11:41:07 -0400

    q&a box

--- app/screens/index.tsx
+++ app/screens/index.tsx
@@ -1,13 +1,28 @@
-import { View, Text, ScrollView } from 'react-native'
+import { View, Text, ScrollView, StyleSheet } from 'react-native'
 import React from 'react'
+import qaBox from '../components/qaBox'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import QaBox from '../components/qaBox'
 
 const HomePage = () => {
   return (
-    <SafeAreaView>
-      <Text>tommy</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Welcome to the home page</Text>
+      <ScrollView contentContainerStyle={{flex: 1, alignItems: 'center', marginTop: 10}}>
+        <QaBox title='samir' content='kanbar' question='Is Jar Jar Binks a Sith lord'/>
+      </ScrollView>
     </SafeAreaView>
   )
 }
 
 export default HomePage
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10
+  },
+  title: {
+    fontSize: 25
+  }
+});
