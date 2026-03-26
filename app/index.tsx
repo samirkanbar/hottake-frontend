43882d26d7119bff6cd6c850ae71721237c928a3
Samir Kanbar <sam.kbr06@gmail.com>
2026-03-26 18:47:56 -0400

    removed import

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,6 @@ import { View, Text, StyleSheet, TextInput, Touchable, TouchableOpacity, Button 
 import {SafeAreaView} from 'react-native-safe-area-context';
 import React, { useState } from 'react';
 import Tabs from './(tabs)/_layout';
-import createpost from './screens/createpost';
-import CreatePost from './screens/createpost';
 
 const index = () => {
   return <Tabs />
